b412fb363eb31a05adb26f037a62ae397286111b
NicolasObama Practica Git <noo0003@alu.medac.es>
2025-06-13 11:36:06 +0200

    Añadida cuenta nueva secundaria1

--- CuentaBancaria.java
+++ CuentaBancaria.java
@@ -1,8 +1,9 @@
 class TestCuentaBancaria
 {
-	public static void main(String args[])
+	public static void main(String args[i] )
 	{
 		CuentaBancaria cuenta = new CuentaBancaria("1234521","Director Banco",100,0);
+		CuentaBancaria cuentaNueva= new CuentaBancaria("0123456789","Nicolas Obama Obiang",1000,5);
 		System.out.println("Saldo = " + cuenta.getSaldo());
 		cuenta.reintegro(50);
 		cuenta.ingreso(100);
